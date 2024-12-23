daed70ba2fa0a6538181386d69446925d11391b6
rajeshkumarpathak01 <rajprofessional42@gmail.com>
2024-12-23 22:55:57 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/currencyConverter/",
-  build: {
-    outDir: 'dist',
-  },
-  plugins: [react()]
+  plugins: [react()],
+  base: "/currencyConverter",
  
 })
